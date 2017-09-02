bdfcaca507e01dce415924c50ef12c1549d2315e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-09-02 15:31:40 -0700

    replacing with new project

---
+++ src/Animal.Web/AnimalData.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Animal.Web
+{
+    public class AnimalData
+    {
+        public string Audio { get; set; }
+        public string Image { get; set; }
+        public string Text { get; set; }
+        public string Name { get; set; }
+
+        public static List<AnimalData> GetFromJson(string json)
+        {
+            return JsonConvert.DeserializeObject<List<AnimalData>>(json);
+        }
+    }
+}
---
+++ src/Animal.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace Animal.Web.Pages {
+    public class IndexModel : PageModel {
+
+        public IndexModel(IHostingEnvironment hostingEnvironment) : base() {
+            _hostingEnvironment = hostingEnvironment;
+            _webRoot = _hostingEnvironment.WebRootPath;
+            SetFilepath("animals.json");
+        }
+        private IHostingEnvironment _hostingEnvironment;
+        private static string _webRoot { get; set; }
+        private static string Filepath { get; set; }
+        private static List<AnimalData> Animals { get; set; }
+        public static void SetFilepath(string filename) {
+            string filepath = Path.Combine(_webRoot, filename);
+            Filepath = filepath;
+        }
+
+        public List<AnimalData> AnimalList { get; set; }
+        public void OnGet() {
+            if (Animals == null) {
+                lock (Filepath) {
+                    Animals = AnimalData.GetFromJson(System.IO.File.ReadAllText(Filepath));
+                }
+            }
+
+            this.AnimalList = Animals;
+        }
+    }
+}
