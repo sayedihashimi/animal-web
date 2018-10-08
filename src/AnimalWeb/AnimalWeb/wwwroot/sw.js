9905d81b2108c249475bb21723d052c5a4bb570d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-10-07 22:23:39 -0400

    caching files in pwa #22

--- src/AnimalWeb/AnimalWeb/wwwroot/sw.js
+++ src/AnimalWeb/AnimalWeb/wwwroot/sw.js
@@ -1,8 +1,56 @@
-﻿self.addEventListener('activate', function (event) {
+﻿const mainCacheName = 'sonyasapp-01';
+self.addEventListener('activate', function (event) {
     event.waitUntil(self.clients.claim()); // Become available to all pages
 });
 
-
 self.addEventListener('fetch', async event => {
-   //handleFetch(event);
+    handleFetch(event);
 });
+
+async function handleFetch(event) {
+    const url = new URL(event.request.url);
+
+    var respondWithCacheFirst = true;
+
+    if(url.pathname.endsWith('.css') || url.pathname.endsWith('.js') || url.pathname === "/"){
+        respondWithCacheFirst = false;
+    }
+
+    respondWithCacheFirst ? 
+        event.respondWith(cacheFirst(event.request)) : 
+        event.respondWith(networkFirst(event.request));
+
+}
+
+async function cacheFirst(request) {
+    try {
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
+            return cachedResponse
+        }
+    }
+    catch (error) {
+        console.debug('error in cacheFirst():'+error);
+    }
+
+    var response = await fetch(request);
+    const cache = await caches.open(mainCacheName);
+    cache.put(request, response.clone());
+
+    return response;
+}
+
+async function networkFirst(request) {
+    console.log('networkFirst');
+    const cache = await caches.open(mainCacheName);
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    }
+    catch (error) {
+        return cache.match(request);
+    }
+}
+
