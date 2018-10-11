91ca09b7a9c13e786a781efc905be1e2b519d622
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-10-10 23:17:19 -0400

    changing animal names

---
+++ src/AnimalWeb/AnimalWeb/gulpfile.js
@@ -0,0 +1,11 @@
+﻿
+// https://www.webstoemp.com/blog/responsive-images-pipeline-with-gulp/
+const gulp = require("gulp");
+const deleteEmpty = require("delete-empty");
+const globby = require("globby");
+const gulp = require("gulp");
+const gulpImgageMin = require("gulp-imagemin");
+const gulpImageresize = require("gulp-image-resize");
+const gulpNewer = require("gulp-newer");
+const merge2 = require("merge2");
+
