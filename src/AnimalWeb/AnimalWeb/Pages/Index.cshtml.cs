67e55c80bfed774147968ffe21fd1c6112695de6
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-10-14 21:48:07 -0400

    updating index page

--- src/AnimalWeb/AnimalWeb/Pages/Index.cshtml.cs
+++ src/AnimalWeb/AnimalWeb/Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using AnimalWeb.Shared;
@@ -17,6 +18,20 @@ namespace AnimalWeb.Pages {
         public List<Animal> Animals {
             get;
             set;
+        }
+
+        public List<string>GetImagesFor(Animal animal) {
+            var result = new List<string>();
+
+            var sizes = new int[]{ 640, 768, 1024, 1366, 1600, 1920, };
+
+            foreach(var size in sizes) {
+                FileInfo fi = new FileInfo(animal.ImageFull);
+                string basename = fi.Name.Substring(0, fi.Name.Length - fi.Extension.Length);
+                result.Add($"{basename}-{size}px{fi.Extension}");
+            }
+
+            return result;
         }
 
         public void OnGet() {
